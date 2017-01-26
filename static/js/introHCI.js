a6e1a0f6d724fa69840f04282c74a401836541a3
Kellie Higa <kkhiga@ucsd.edu>
2017-01-26 10:52:38 -0800

    I don't know man

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,39 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+	$("#testjs").click(
+		function(e) {
+			$('.jumbotron h1').text("Javascript is connected");
+		}
+	);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("#userName").click(changeText);
+
+	$('.project').click(projectClick);
+	
+
+}
+
+
+/* Project Click Function - Adds Project Description */
+// function projectClick() {
+//     var containingProject = $(this).closest(".project");
+//     containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+// }
+
+function changeText(event) {
+	$(this).text("Changed Text");
+}
+
+function projectClick(event) {
+	var containingProject = $(this).closest(".project");
+	containingProject.append("<div class = 'project-description'><p>Description of the project.</p></div>");
 }
